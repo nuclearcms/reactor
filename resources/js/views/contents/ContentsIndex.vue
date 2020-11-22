34ee1b71411bcfa96ed2c1ee9cc694f5cf83a375
Egemen Berker Kızılcan <egemen@umomega.com>
2020-11-22 20:33:20 +0300

    Separated contents datable into single view
    And added content cover thumbnails

--- resources/js/views/contents/ContentsIndex.vue
+++ resources/js/views/contents/ContentsIndex.vue
@@ -1,22 +1,5 @@
 <template>
-	<Datable
-		:defaultkey="'created_at'" :defaultdir="'desc'"
-		:route="'contents.index'"
-		:sortables="[
-			{key: 'title', label:'validation.attributes.title'},
-			{key: 'type', label:'validation.attributes.type', skip: true},
-			{key: 'published_at', label:'validation.attributes.published_at'},
-			{key: 'created_at', label:'validation.attributes.created_at'}
-		]"
-		:createroutename="'contents.create'"
-		:createicon="'plus'"
-		:indexloadroute="'contents'"
-		:searchloadroute="'contents/search'"
-		:bulkdeleteroute="'contents/bulk'"
-		:alllabel="'hierarchy::contents.all'"
-		:canwrite="$can('write_contents')"
-		:filterable="true"
-		:filters="[
+	<ContentsDatable :route="'contents.index'" :createroutename="'contents.create'" :createicon="'plus'" :indexloadroute="'contents'" :searchloadroute="'contents/search'" :bulkdeleteroute="'contents/bulk'" :alllabel="'hierarchy::contents.all'" :hidestoolbar="false" :filterable="true" :filters="[
 			{value: 'unpublished', label: trans.get('hierarchy::contents.s_unpublished')},
 			{value: 'locked', label: trans.get('hierarchy::contents.s_locked')},
 			{value: 'invisible', label: trans.get('hierarchy::contents.s_invisible')},
@@ -24,40 +7,19 @@
 			{value: 'archived', label: trans.get('hierarchy::contents.s_archived')},
 			{value: 'published', label: trans.get('hierarchy::contents.s_published')}
 		]"
-	>
-		<template v-slot:table-row-thumb="slotProps">
-			<div class="datable-row-thumb">
-				IMG
-			</div>
-		</template>
-
-		<template v-slot:default="slotProps">
-			<td><router-link :to="{ name: (slotProps.row.hides_children || slotProps.row.content_type.hides_children ? 'contents.children' : 'contents.edit'), params: {id: slotProps.row.id} }" v-text="slotProps.row.title[$root.appLocale]" /></td>
-			<td><router-link class="is-size-7" v-if="$can('read_contenttypes')" :to="{ name: 'contenttypes.edit', params: { id: slotProps.row.content_type_id }}" v-text="slotProps.row.content_type.name"/><span class="is-size-7 has-text-grey-darker" v-else v-text="slotProps.row.content_type.name"></span></td>
-			<td><span class="is-size-7" v-text="makeReadableDate(slotProps.row.published_at)"></span></td>
-			<td><span class="is-size-7" v-text="makeReadableDate(slotProps.row.created_at)"></span></td>
-			<td class="datable-row-options">
-				<ContentsDropdown :can="$can('write_contents')" :content="slotProps.row"/>
-			</td>
-		</template>
-	</Datable>
+	/>
 </template>
 
 <script>
-import {View, Datable, format_date, RequiresPermissions} from 'umomega-foundation'
-import ContentsDropdown from '../../partials/ContentsDropdown'
+import {View} from 'umomega-foundation'
+import ContentsDatable from '../../partials/ContentsDatable'
 
 export default {
-	mixins: [ View, RequiresPermissions ],
-	components: { Datable, ContentsDropdown },
+	mixins: [ View ],
+	components: { ContentsDatable },
 	data() { return {
 		titleLabel: 'hierarchy::contents.manage',
 		guardedBy: 'read_contents'
-	}},
-	methods: {
-		makeReadableDate(date) {
-			return format_date(date);
-		}
-	}
+	}}
 }
 </script>
--- resources/js/views/tags/TagsContents.vue
+++ resources/js/views/tags/TagsContents.vue
@@ -11,47 +11,19 @@
 		]"></tabs>
 
 		<div class="paper">
-			<Datable
-				:defaultkey="'created_at'" :defaultdir="'desc'"
-				:route="'tags.contents'"
-				:sortables="[
-					{key: 'title', label:'validation.attributes.title'},
-					{key: 'type', label:'validation.attributes.type', skip: true},
-					{key: 'published_at', label:'validation.attributes.published_at'},
-					{key: 'created_at', label:'validation.attributes.created_at'}
-				]"
-				:indexloadroute="'tags/' + $route.params.id + '/contents'"
-				:canwrite="$can('write_contents')"
-				:hidestoolbar="true"
-			>
-				<template v-slot:table-row-thumb="slotProps">
-					<div class="datable-row-thumb">
-						IMG
-					</div>
-				</template>
-
-				<template v-slot:default="slotProps">
-					<td><router-link :to="{ name: (slotProps.row.hides_children || slotProps.row.content_type.hides_children ? 'contents.children' : 'contents.edit'), params: {id: slotProps.row.id} }" v-text="slotProps.row.title[$root.appLocale]" /></td>
-					<td><router-link class="is-size-7" v-if="$can('read_contenttypes')" :to="{ name: 'contenttypes.edit', params: { id: slotProps.row.content_type_id }}" v-text="slotProps.row.content_type.name"/><span class="is-size-7 has-text-grey-darker" v-else v-text="slotProps.row.content_type.name"></span></td>
-					<td><span class="is-size-7" v-text="makeReadableDate(slotProps.row.published_at)"></span></td>
-					<td><span class="is-size-7" v-text="makeReadableDate(slotProps.row.created_at)"></span></td>
-					<td class="datable-row-options">
-						<ContentsDropdown :can="$can('write_contents')" :content="slotProps.row"/>
-					</td>
-				</template>
-			</Datable>
+			<ContentsDatable :route="'tags.contents'" :indexloadroute="'tags/' + $route.params.id + '/contents'" :hidestoolbar="true"/>
 		</div>
 	</div>
 </template>
 
 <script>
-import {View, Shower, Tabs, Datable, format_date, RequiresPermissions, assess_error} from 'umomega-foundation'
-import ContentsDropdown from '../../partials/ContentsDropdown'
+import {View, Shower, Tabs} from 'umomega-foundation'
+import ContentsDatable from '../../partials/ContentsDatable'
 import {TagsEditHelper} from 'umomega-tags'
 
 export default {
-	mixins: [ View, Shower, RequiresPermissions, TagsEditHelper ],
-	components: { Datable, Tabs, ContentsDropdown },
+	mixins: [ View, Shower, TagsEditHelper ],
+	components: { Tabs, ContentsDatable },
 	data() { return {
 		titleLabel: 'hierarchy::contents.multiple',
 		breadcrumbs: [
@@ -59,11 +31,6 @@ export default {
 		],
 		showRoute: 'tags',
 		guardedBy: 'read_tags'
-	}},
-	methods: {
-		makeReadableDate(date) {
-			return format_date(date);
-		}
-	}
+	}}
 }
 </script>

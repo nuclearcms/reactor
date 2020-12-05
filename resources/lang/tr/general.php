c80e71480b7b088f8f8eb535e698ff93e9cd5b56
Egemen Berker Kızılcan <egemen@umomega.com>
2020-12-05 19:06:02 +0300

    Added Turkish translations

---
+++ resources/lang/tr/general.php
@@ -0,0 +1,60 @@
+<?php
+
+return [
+	'contents' => 'İçerikler',
+	'dashboard' => 'Genel Bakış',
+	'dashboard_description' => 'İşte web siteniz ve içeriğinizle ilgili bazı yeni etkinlikler.',
+	'construction_maintenance' => 'Yapım ve Bakım',
+
+	'environment_variables' => 'Çevre Değişkenleri',
+	'manage_environment' => 'Çevre Değişkenlerini Yönet',
+	'danger_zone' => 'Tehlike Bölgesi!',
+	'danger_zone_warning' => 'Buradaki ayarlardan herhangi birini değiştirerek web sitenize kalıcı olarak zarar verebilirsiniz. Dikkatle ilerleyin veya web yöneticinizden yardım isteyin.',
+	'manage_logs' => 'Günlükler',
+	'cleared_logs' => 'Günlükler temizlendi.',
+
+	'maintenance' => 'Bakım',
+	'maintain_reactor' => 'Reactor\'a Bakım Yap',
+	'maintenance_command_not_found' => 'Bakım komutu bulunamadı.',
+	'maintenance_command_run_successfully' => 'Bakım komutu başarıyla yürütüldü.',
+
+	'maintenance_clear-compiled' => 'Derlenmiş sınıf dosyasını kaldırma',
+	'maintenance_down' => 'Uygulamayı bakım / demo moduna geçirme',
+	'maintenance_optimize' => 'Uygulama önyükleme dosyalarını önbelleğe alın',
+	'maintenance_activitylog_clean' => 'Etkinlik günlüğünden eski kayıtları temizleme',
+	'maintenance_cache_clear' => 'Uygulama önbelleğini temizleme',
+	'maintenance_config_cache' => ' Daha hızlı yapılandırma yüklemesi için bir önbellek dosyası oluşturma',
+	'maintenance_config_clear' => 'Yapılandırma önbellek dosyasını kaldırma',
+	'maintenance_key_generate' => 'Uygulama anahtarını güncelleme',
+	'maintenance_optimize_clear' => 'Önbelleğe alınmış önyükleme dosyalarını kaldırma',
+	'maintenance_package_discover' => 'Önbelleğe alınmış paket bildirimini yeniden oluşturma',
+	'maintenance_permission_cache-reset' => 'İzin önbelleğini sıfırlama',
+	'maintenance_route_cache' => 'Daha hızlı rota kaydı için bir rota önbellek dosyası oluşturma',
+	'maintenance_route_clear' => 'Rota önbellek dosyasını kaldırma',
+	'maintenance_view_cache' => 'Uygulamanın tüm Blade şablonlarını derleme',
+	'maintenance_view_clear' => 'Tüm derlenmiş görünüm dosyalarını temizleme',
+	'maintenance_sitemap_generate' => 'Site haritasını oluşturma',
+	'maintenance_telescope_clear' => 'Telescope\'taki tüm girişleri temizleme',
+
+	'section_maintenance' => 'Bakım',
+	'section_maintenance_desc' => 'Genel bakım komutları',
+	'section_performance' => 'Performans',
+	'section_performance_desc' => 'Uygulama performansını artıran önbelleğe alma komutları',
+	'section_cleanup' => 'Temizlik',
+	'section_cleanup_desc' => 'Uygulama önbelleklerini temizleyen temizleme komutları',
+	'section_seo' => 'SEO (Arama Motoru Optimizasyonu)',
+	'section_seo_desc' => 'Uygulamanın SEO yeteneklerini geliştirmek için komutlar',
+	'section_security' => 'Güvenlik',
+	'section_security_desc' => 'Uygulamanın güvenliğini sağlamak için komutlar',
+
+	'per_last7' => 'Son 7 Gün',
+	'per_last28' => 'Son 28 Gün',
+	'total_views' => 'Toplam Görüntülenme',
+	'views_today' => 'Bugünkü Görüntülenme',
+	'latest_view' => 'En Son Görüntülenme',
+
+	'most_viewed' => 'En Çok Görüntülenen',
+	'recently_viewed' => 'En Yeni Görüntülenen',
+	'recently_updated' => 'En Yeni Güncellenen',
+	'recently_created' => 'En Yeni Oluşturulan'
+];

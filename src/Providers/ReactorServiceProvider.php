d6ed898a3c0addf9bda76995a1ef2bfb45990107
Egemen Berker Kızılcan <egemen@umomega.com>
2020-12-07 15:21:26 +0300

    Updated version number

--- src/Providers/ReactorServiceProvider.php
+++ src/Providers/ReactorServiceProvider.php
@@ -11,7 +11,7 @@ class ReactorServiceProvider extends ServiceProvider
     /**
      * Nuclear Version
      */
-    const VERSION = '4.0.2';
+    const VERSION = '4.0.4';
 
     /**
      * Register any application services.

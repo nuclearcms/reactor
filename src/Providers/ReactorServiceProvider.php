76006dabf4b8ba12400dcece4657ea9cb76f4ad7
Egemen Berker Kızılcan <egemenkizilcan@gmail.com>
2022-06-03 11:49:34 +0100

    Updated Laravel Mix to V6

--- src/Providers/ReactorServiceProvider.php
+++ src/Providers/ReactorServiceProvider.php
@@ -10,7 +10,7 @@ class ReactorServiceProvider extends ServiceProvider
     /**
      * Nuclear Version
      */
-    const VERSION = '4.2.5';
+    const VERSION = '4.2.6';
 
     /**
      * Register any application services.
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
 const mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.js', 'public/js').vue({ version: 2 })
     .sass('resources/sass/app.sass', 'public/css')
     .copy('public', '../../public/vendor/app')
     .options({

e0dad57584b6c64a7133b130b2b25611bca474ef
Egemen Berker Kızılcan <egemenkizilcan@gmail.com>
2022-07-29 14:57:18 +0100

    Update ReactorServiceProvider.php

--- src/Providers/ReactorServiceProvider.php
+++ src/Providers/ReactorServiceProvider.php
@@ -10,7 +10,7 @@ class ReactorServiceProvider extends ServiceProvider
     /**
      * Nuclear Version
      */
-    const VERSION = '4.2.6';
+    const VERSION = '4.2.7';
 
     /**
      * Register any application services.

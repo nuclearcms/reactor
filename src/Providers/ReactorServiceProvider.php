18d6fb3382b2f3bdb2887e271e8ff71d784dcc79
Egemen Berker Kızılcan <egemenkizilcan@gmail.com>
2023-04-04 19:54:29 +0100

    Soft Deleting Models & Bugfixes

--- src/Providers/ReactorServiceProvider.php
+++ src/Providers/ReactorServiceProvider.php
@@ -10,7 +10,7 @@ class ReactorServiceProvider extends ServiceProvider
     /**
      * Nuclear Version
      */
-    const VERSION = '4.5.1';
+    const VERSION = '4.7.0';
 
     /**
      * Register any application services.
